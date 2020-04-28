f9d89dd1927df30efd25f6bc90d7e5c853de6294
AhlaamK-tap <a.kazi@tap.company>
2020-04-28 17:02:11 +0300

    chipTheme

---
+++ thememanager/src/main/java/company/tap/thememanager/theme/ChipTheme.kt
@@ -0,0 +1,16 @@
+package company.tap.thememanager.theme
+
+/**
+ * Created by AhlaamK on 4/28/20.
+
+Copyright (c) 2020    Tap Payments.
+All rights reserved.
+ **/
+open class ChipTheme : BaseTextTheme(){
+    var cardCornerRadius: Int? = null
+    var cardPreventCornerOverlap: Boolean = false
+    var outlineSpotShadowColor: Int? = null
+    var ContentPadding :Int?=null
+    var maxCardElevation:Int?=null
+    var cardElevation:Int?=null
+}
