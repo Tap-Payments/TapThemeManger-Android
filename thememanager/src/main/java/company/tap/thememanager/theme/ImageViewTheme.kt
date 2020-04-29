f26ecfc9bc7a024bf474e323c6586207708f9dfc
AhlaamK-tap <a.kazi@tap.company>
2020-04-29 09:44:15 +0300

    ImageView theme added

---
+++ thememanager/src/main/java/company/tap/thememanager/theme/ImageViewTheme.kt
@@ -0,0 +1,10 @@
+package company.tap.thememanager.theme
+
+/**
+ * Created by AhlaamK on 4/29/20.
+
+Copyright (c) 2020    Tap Payments.
+All rights reserved.
+ **/
+open class ImageViewTheme :BaseTextTheme(){
+}
