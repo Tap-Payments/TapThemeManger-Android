945bce5d007c67dfb3549ae20216469fc4755d00
olamonir <ola.monir7@gmail.com>
2020-08-25 22:28:02 +0300

    Add missing attributes for theming

--- thememanager/src/main/java/company/tap/thememanager/theme/ButtonTheme.kt
+++ thememanager/src/main/java/company/tap/thememanager/theme/ButtonTheme.kt
@@ -4,4 +4,10 @@ package company.tap.thememanager.theme
  * Created by Mario Gamal on 4/20/20
  * Copyright © 2020 Tap Payments. All rights reserved.
  */
-open class ButtonTheme : BaseTextTheme()
+open class ButtonTheme : BaseTextTheme(){
+    var font : String? = null
+    var color : String? = null
+    var backgroundColor :String? =null
+    var borderColor :String? =null
+
+}
