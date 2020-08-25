e1f0c5d80d98d7d8d0c1f5f39103b7234b9f0b0d
AhlaamK-tap <a.kazi@tap.company>
2020-08-25 13:05:44 +0300

    TabSelectTheme added

---
+++ thememanager/src/main/java/company/tap/thememanager/theme/TabSelectTheme.kt
@@ -0,0 +1,14 @@
+package company.tap.thememanager.theme
+
+/**
+ * Created by AhlaamK on 8/25/20.
+
+Copyright (c) 2020    Tap Payments.
+All rights reserved.
+ **/
+open class TabSelectTheme : BaseTextTheme() {
+    var backgroundColor: Int? = null
+    var selectedBackgroundColor:Unit?= null
+    var unselectedBackgroundColor:Unit?= null
+}
+
